22242479dfeba49604d2607348d1f520c2daa3e2
RaiyanAziz55 <raiyanaziz88@gmail.com>
2023-10-02 17:42:25 -0400

    added files

---
+++ CyclingAdministration/app/src/main/java/com/example/cyclingadministration/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cyclingadministration;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.FirebaseApp;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+    }
+}
